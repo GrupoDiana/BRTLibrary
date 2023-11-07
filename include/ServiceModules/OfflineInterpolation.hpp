d5152a2db9cf838a187df69fa8eba7b5df71ceab
Lmtanco <lmtanco@users.noreply.github.com>
2023-11-07 11:36:50 +0100

    make sure method returns a value in all cases

--- include/ServiceModules/OfflineInterpolation.hpp
+++ include/ServiceModules/OfflineInterpolation.hpp
@@ -17,7 +17,7 @@
 *
 * \b Licence: This program is free software, you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 *
-* \b Acknowledgement: This project has received funding from the European Union’s Horizon 2020 research and innovation programme under grant agreement no.101017743
+* \b Acknowledgement: This project has received funding from the European Unionï¿½s Horizon 2020 research and innovation programme under grant agreement no.101017743
 */
 
 
@@ -164,6 +164,7 @@ namespace BRTServices
 					}
 				}
 			}
+			return newHRIR;
 			//SET_RESULT(RESULT_OK, "");			
 		}
 		friend class CHRTFTester;
