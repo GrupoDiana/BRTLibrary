9edd907fbfdb4258a336971c4fb36b82178d08f6
Daniel González-Toledo <dgonzalezt@uma.es>
2023-11-21 13:14:28 +0100

    feat: Unifying common methods

---
+++ include/Common/AddDelayExpansionMethod.hpp
@@ -0,0 +1,122 @@
+/**
+* \class CAddDelayExpansionMethod
+*
+* \brief Declaration of CAddDelayExpansionMethod class interface.
+* \date	November 2023
+*
+* \authors 3DI-DIANA Research Group (University of Malaga), in alphabetical order: M. Cuevas-Rodriguez, D. Gonzalez-Toledo, L. Molina-Tanco, F. Morales-Benitez ||
+* Coordinated by , A. Reyes-Lecuona (University of Malaga)||
+* \b Contact: areyes@uma.es
+*
+* \b Contributions: (additional authors/contributors can be added here)
+*
+* \b Project: SONICOM ||
+* \b Website: https://www.sonicom.eu/
+*
+* \b Copyright: University of Malaga 2023. Code based in the 3DTI Toolkit library (https://github.com/3DTune-In/3dti_AudioToolkit) with Copyright University of Malaga and Imperial College London - 2018
+*
+* \b Licence: This program is free software, you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+*
+* \b Acknowledgement: This project has received funding from the European Union’s Horizon 2020 research and innovation programme under grant agreement no.101017743
+*/
+
+#ifndef _CADD_DELAY_EXPANSION_METHOD_HPP_
+#define _CADD_DELAY_EXPANSION_METHOD_HPP_
+
+#include <Common/Buffer.hpp>
+
+namespace Common {
+
+	class CAddDelayExpansionMethod {
+	public:
+
+		
+		/**
+		 * @brief Adds or removes a delay to a buffer, by doing an expansion or compression. 
+		 * @param input Buffer to be processed, to which the delay is to be applied
+		 * @param output Result of processing
+		 * @param delayBuffer buffer containing the information of the last frame. This buffer will also be updated.
+		 * @param newDelay Delay to be applied
+		*/
+		static void ProcessAddDelay_ExpansionMethod(CMonoBuffer<float>& input, CMonoBuffer<float>& output, CMonoBuffer<float>& delayBuffer, int newDelay)
+		{
+			//Prepare the outbuffer		
+			if (output.size() != input.size()) { output.resize(input.size()); }
+
+			//Prepare algorithm variables
+			float position = 0;
+			float numerator = input.size() - 1;
+			float denominator = input.size() - 1 + newDelay - delayBuffer.size();
+			float compressionFactor = numerator / denominator;
+
+			//Add samples to the output from buffer
+			for (int i = 0; i < delayBuffer.size(); i++)
+			{
+				output[i] = delayBuffer[i];
+			}
+
+			//Fill the others buffers
+			//if the delay is the same one as the previous frame use a simplification of the algorithm
+			if (newDelay == delayBuffer.size())
+			{
+				//Copy input to output
+				int j = 0;
+				for (int i = delayBuffer.size(); i < input.size(); i++)
+				{
+					output[i] = input[j++];
+				}
+				//Fill delay buffer
+				for (int i = 0; i < newDelay; i++)
+				{
+					delayBuffer[i] = input[j++];
+				}
+			}
+			//else, apply the expansion/compression algorihtm
+			else
+			{
+				int j;
+				float rest;
+				int forLoop_end;
+				//The last loop iteration must be addressed in a special way if newDelay = 0 (part 1)
+				if (newDelay == 0) { forLoop_end = input.size() - 1; }
+				else { forLoop_end = input.size(); }
+
+				//Fill the output buffer with the new values 
+				for (int i = delayBuffer.size(); i < forLoop_end; i++)
+				{
+					j = static_cast<int>(position);
+					rest = position - j;
+					output[i] = input[j] * (1 - rest) + input[j + 1] * rest;
+					position += compressionFactor;
+				}
+
+				//The last loop iteration must be addressed in a special way if newDelay = 0 (part 2)
+				if (newDelay == 0)
+				{
+					output[input.size() - 1] = input[input.size() - 1];
+					delayBuffer.clear();
+				}
+
+				//if newDelay!=0 fill out the delay buffer
+				else
+				{
+					//Fill delay buffer 			
+					CMonoBuffer<float> temp;
+					temp.reserve(newDelay);
+					for (int i = 0; i < newDelay - 1; i++)
+					{
+						int j = int(position);
+						float rest = position - j;
+						temp.push_back(input[j] * (1 - rest) + input[j + 1] * rest);
+						position += compressionFactor;
+					}
+					//Last element of the delay buffer that must be addressed in a special way
+					temp.push_back(input[input.size() - 1]);
+					//delayBuffer.swap(temp);				//To use in C++03
+					delayBuffer = std::move(temp);			//To use in C++11
+				}
+			}
+		}
+	};
+}
+#endif
--- include/ProcessingModules/HRTFConvolver.hpp
+++ include/ProcessingModules/HRTFConvolver.hpp
@@ -25,15 +25,17 @@
 
 #include <Common/UPCAnechoic.hpp>
 #include <Common/Buffer.hpp>
+#include <Common/AddDelayExpansionMethod.hpp>
+#include <Common/SourceListenerRelativePositionCalculation.hpp>
 #include <ServiceModules/HRTF.hpp>
 
 #include <memory>
 #include <vector>
 #include <algorithm>
 
-#define EPSILON 0.0001f
-#define ELEVATION_SINGULAR_POINT_UP 90.0
-#define ELEVATION_SINGULAR_POINT_DOWN 270.0
+//#define EPSILON 0.0001f
+//#define ELEVATION_SINGULAR_POINT_UP 90.0
+//#define ELEVATION_SINGULAR_POINT_DOWN 270.0
 
 namespace BRTProcessing {
 	class CHRTFConvolver  {
@@ -109,7 +111,7 @@ namespace BRTProcessing {
 			float centerElevation;
 			float interauralAzimuth;
 
-			CalculateSourceCoordinates(sourceTransform, listenerTransform, _listenerHRTF, leftElevation, leftAzimuth, rightElevation, rightAzimuth, centerElevation, centerAzimuth, interauralAzimuth);
+			Common::CSourceListenerRelativePositionCalculation::CalculateSourceListenerRelativePositions(sourceTransform, listenerTransform, _listenerHRTF, leftElevation, leftAzimuth, rightElevation, rightAzimuth, centerElevation, centerAzimuth, interauralAzimuth);
 
 			// GET HRTF
 			std::vector<CMonoBuffer<float>>  leftHRIR_partitioned;
@@ -137,8 +139,8 @@ namespace BRTProcessing {
 			//CMonoBuffer<float> outLeftBuffer;
 			//CMonoBuffer<float> outRightBuffer;
 
-			ProcessAddDelay_ExpansionMethod(leftChannel_withoutDelay, outLeftBuffer, leftChannelDelayBuffer, leftDelay);
-			ProcessAddDelay_ExpansionMethod(rightChannel_withoutDelay, outRightBuffer, rightChannelDelayBuffer, rightDelay);
+			Common::CAddDelayExpansionMethod::ProcessAddDelay_ExpansionMethod(leftChannel_withoutDelay, outLeftBuffer, leftChannelDelayBuffer, leftDelay);
+			Common::CAddDelayExpansionMethod::ProcessAddDelay_ExpansionMethod(rightChannel_withoutDelay, outRightBuffer, rightChannelDelayBuffer, rightDelay);
 
 			// Propagate Result            
 			//GetSamplesExitPoint("leftEar")->sendData(outLeftBuffer);
@@ -175,85 +177,85 @@ namespace BRTProcessing {
 		/////////////////////
 
 		// Apply doppler effect simulation
-		void ProcessAddDelay_ExpansionMethod(CMonoBuffer<float>& input, CMonoBuffer<float>& output, CMonoBuffer<float>& delayBuffer, int newDelay)
-		{
-			//Prepare the outbuffer		
-			if (output.size() != input.size()) { output.resize(input.size()); }
+		//void ProcessAddDelay_ExpansionMethod(CMonoBuffer<float>& input, CMonoBuffer<float>& output, CMonoBuffer<float>& delayBuffer, int newDelay)
+		//{
+		//	//Prepare the outbuffer		
+		//	if (output.size() != input.size()) { output.resize(input.size()); }
 
-			//Prepare algorithm variables
-			float position = 0;
-			float numerator = input.size() - 1;
-			float denominator = input.size() - 1 + newDelay - delayBuffer.size();
-			float compressionFactor = numerator / denominator;
+		//	//Prepare algorithm variables
+		//	float position = 0;
+		//	float numerator = input.size() - 1;
+		//	float denominator = input.size() - 1 + newDelay - delayBuffer.size();
+		//	float compressionFactor = numerator / denominator;
 
-			//Add samples to the output from buffer
-			for (int i = 0; i < delayBuffer.size(); i++)
-			{
-				output[i] = delayBuffer[i];
-			}
+		//	//Add samples to the output from buffer
+		//	for (int i = 0; i < delayBuffer.size(); i++)
+		//	{
+		//		output[i] = delayBuffer[i];
+		//	}
 
-			//Fill the others buffers
-			//if the delay is the same one as the previous frame use a simplification of the algorithm
-			if (newDelay == delayBuffer.size())
-			{
-				//Copy input to output
-				int j = 0;
-				for (int i = delayBuffer.size(); i < input.size(); i++)
-				{
-					output[i] = input[j++];
-				}
-				//Fill delay buffer
-				for (int i = 0; i < newDelay; i++)
-				{
-					delayBuffer[i] = input[j++];
-				}
-			}
-			//else, apply the expansion/compression algorihtm
-			else
-			{
-				int j;
-				float rest;
-				int forLoop_end;
-				//The last loop iteration must be addressed in a special way if newDelay = 0 (part 1)
-				if (newDelay == 0) { forLoop_end = input.size() - 1; }
-				else { forLoop_end = input.size(); }
+		//	//Fill the others buffers
+		//	//if the delay is the same one as the previous frame use a simplification of the algorithm
+		//	if (newDelay == delayBuffer.size())
+		//	{
+		//		//Copy input to output
+		//		int j = 0;
+		//		for (int i = delayBuffer.size(); i < input.size(); i++)
+		//		{
+		//			output[i] = input[j++];
+		//		}
+		//		//Fill delay buffer
+		//		for (int i = 0; i < newDelay; i++)
+		//		{
+		//			delayBuffer[i] = input[j++];
+		//		}
+		//	}
+		//	//else, apply the expansion/compression algorihtm
+		//	else
+		//	{
+		//		int j;
+		//		float rest;
+		//		int forLoop_end;
+		//		//The last loop iteration must be addressed in a special way if newDelay = 0 (part 1)
+		//		if (newDelay == 0) { forLoop_end = input.size() - 1; }
+		//		else { forLoop_end = input.size(); }
 
-				//Fill the output buffer with the new values 
-				for (int i = delayBuffer.size(); i < forLoop_end; i++)
-				{
-					j = static_cast<int>(position);
-					rest = position - j;
-					output[i] = input[j] * (1 - rest) + input[j + 1] * rest;
-					position += compressionFactor;
-				}
+		//		//Fill the output buffer with the new values 
+		//		for (int i = delayBuffer.size(); i < forLoop_end; i++)
+		//		{
+		//			j = static_cast<int>(position);
+		//			rest = position - j;
+		//			output[i] = input[j] * (1 - rest) + input[j + 1] * rest;
+		//			position += compressionFactor;
+		//		}
 
-				//The last loop iteration must be addressed in a special way if newDelay = 0 (part 2)
-				if (newDelay == 0)
-				{
-					output[input.size() - 1] = input[input.size() - 1];
-					delayBuffer.clear();
-				}
+		//		//The last loop iteration must be addressed in a special way if newDelay = 0 (part 2)
+		//		if (newDelay == 0)
+		//		{
+		//			output[input.size() - 1] = input[input.size() - 1];
+		//			delayBuffer.clear();
+		//		}
 
-				//if newDelay!=0 fill out the delay buffer
-				else
-				{
-					//Fill delay buffer 			
-					CMonoBuffer<float> temp;
-					temp.reserve(newDelay);
-					for (int i = 0; i < newDelay - 1; i++)
-					{
-						int j = int(position);
-						float rest = position - j;
-						temp.push_back(input[j] * (1 - rest) + input[j + 1] * rest);
-						position += compressionFactor;
-					}
-					//Last element of the delay buffer that must be addressed in a special way
-					temp.push_back(input[input.size() - 1]);
-					//delayBuffer.swap(temp);				//To use in C++03
-					delayBuffer = std::move(temp);			//To use in C++11
-				}
-			}
-		}//End ProcessAddDelay_ExpansionMethod
+		//		//if newDelay!=0 fill out the delay buffer
+		//		else
+		//		{
+		//			//Fill delay buffer 			
+		//			CMonoBuffer<float> temp;
+		//			temp.reserve(newDelay);
+		//			for (int i = 0; i < newDelay - 1; i++)
+		//			{
+		//				int j = int(position);
+		//				float rest = position - j;
+		//				temp.push_back(input[j] * (1 - rest) + input[j + 1] * rest);
+		//				position += compressionFactor;
+		//			}
+		//			//Last element of the delay buffer that must be addressed in a special way
+		//			temp.push_back(input[input.size() - 1]);
+		//			//delayBuffer.swap(temp);				//To use in C++03
+		//			delayBuffer = std::move(temp);			//To use in C++11
+		//		}
+		//	}
+		//}//End ProcessAddDelay_ExpansionMethod
 
 
 		/// Initialize convolvers and convolition buffers		
@@ -274,99 +276,86 @@ namespace BRTProcessing {
 		}
 
 
-		/// Calculates the parameters derived from the source and listener position
-		void CalculateSourceCoordinates(Common::CTransform& _sourceTransform, Common::CTransform& _listenerTransform, std::shared_ptr<BRTServices::CHRTF>& _listenerHRTF, float& leftElevation, float& leftAzimuth, float& rightElevation, float& rightAzimuth, float& centerElevation, float& centerAzimuth, float& interauralAzimuth)
-		{
+		///// Calculates the parameters derived from the source and listener position
+		//void CalculateSourceCoordinates(Common::CTransform& _sourceTransform, Common::CTransform& _listenerTransform, std::shared_ptr<BRTServices::CHRTF>& _listenerHRTF, float& leftElevation, float& leftAzimuth, float& rightElevation, float& rightAzimuth, float& centerElevation, float& centerAzimuth, float& interauralAzimuth)
+		//{
 
-			//Get azimuth and elevation between listener and source
-			Common::CVector3 _vectorToListener = _listenerTransform.GetVectorTo(_sourceTransform);
-			float _distanceToListener = _vectorToListener.GetDistance();
+		//	//Get azimuth and elevation between listener and source
+		//	Common::CVector3 _vectorToListener = _listenerTransform.GetVectorTo(_sourceTransform);
+		//	float _distanceToListener = _vectorToListener.GetDistance();
 
-			//Check listener and source are in the same position
-			if (_distanceToListener <= MINIMUM_DISTANCE_SOURCE_LISTENER) {				
-				SET_RESULT(RESULT_WARNING, "The sound source is too close to the centre of the listener's head in BRTProcessing::CHRTFConvolver");
-				_distanceToListener = MINIMUM_DISTANCE_SOURCE_LISTENER;
-			}
+		//	//Check listener and source are in the same position
+		//	if (_distanceToListener <= MINIMUM_DISTANCE_SOURCE_LISTENER) {				
+		//		SET_RESULT(RESULT_WARNING, "The sound source is too close to the centre of the listener's head in BRTProcessing::CHRTFConvolver");
+		//		_distanceToListener = MINIMUM_DISTANCE_SOURCE_LISTENER;
+		//	}
 
-			Common::CVector3 leftEarLocalPosition = _listenerHRTF->GetEarLocalPosition(Common::T_ear::LEFT);
-			Common::CVector3 rightEarLocalPosition = _listenerHRTF->GetEarLocalPosition(Common::T_ear::RIGHT);
-			Common::CTransform leftEarTransform = _listenerTransform.GetLocalTranslation(leftEarLocalPosition);
-			Common::CTransform rightEarTransform = _listenerTransform.GetLocalTranslation(rightEarLocalPosition);
+		//	Common::CVector3 leftEarLocalPosition = _listenerHRTF->GetEarLocalPosition(Common::T_ear::LEFT);
+		//	Common::CVector3 rightEarLocalPosition = _listenerHRTF->GetEarLocalPosition(Common::T_ear::RIGHT);
+		//	Common::CTransform leftEarTransform = _listenerTransform.GetLocalTranslation(leftEarLocalPosition);
+		//	Common::CTransform rightEarTransform = _listenerTransform.GetLocalTranslation(rightEarLocalPosition);
 
-			Common::CVector3 leftVectorTo = leftEarTransform.GetVectorTo(_sourceTransform);
-			Common::CVector3 rightVectorTo = rightEarTransform.GetVectorTo(_sourceTransform);
-			Common::CVector3 leftVectorTo_sphereProjection = GetSphereProjectionPosition(leftVectorTo, leftEarLocalPosition, _listenerHRTF->GetHRTFDistanceOfMeasurement());
-			Common::CVector3 rightVectorTo_sphereProjection = GetSphereProjectionPosition(rightVectorTo, rightEarLocalPosition, _listenerHRTF->GetHRTFDistanceOfMeasurement());
+		//	Common::CVector3 leftVectorTo = leftEarTransform.GetVectorTo(_sourceTransform);
+		//	Common::CVector3 rightVectorTo = rightEarTransform.GetVectorTo(_sourceTransform);
+		//	Common::CVector3 leftVectorTo_sphereProjection = GetSphereProjectionPosition(leftVectorTo, leftEarLocalPosition, _listenerHRTF->GetHRTFDistanceOfMeasurement());
+		//	Common::CVector3 rightVectorTo_sphereProjection = GetSphereProjectionPosition(rightVectorTo, rightEarLocalPosition, _listenerHRTF->GetHRTFDistanceOfMeasurement());
 
-			leftElevation = leftVectorTo_sphereProjection.GetElevationDegrees();	//Get left elevation
-			if (!Common::AreSame(ELEVATION_SINGULAR_POINT_UP, leftElevation, EPSILON) && !Common::AreSame(ELEVATION_SINGULAR_POINT_DOWN, leftElevation, EPSILON))
-			{
-				leftAzimuth = leftVectorTo_sphereProjection.GetAzimuthDegrees();	//Get left azimuth
-			}
+		//	leftElevation = leftVectorTo_sphereProjection.GetElevationDegrees();	//Get left elevation
+		//	if (!Common::AreSame(ELEVATION_SINGULAR_POINT_UP, leftElevation, EPSILON) && !Common::AreSame(ELEVATION_SINGULAR_POINT_DOWN, leftElevation, EPSILON))
+		//	{
+		//		leftAzimuth = leftVectorTo_sphereProjection.GetAzimuthDegrees();	//Get left azimuth
+		//	}
 
-			rightElevation = rightVectorTo_sphereProjection.GetElevationDegrees();	//Get right elevation	
-			if (!Common::AreSame(ELEVATION_SINGULAR_POINT_UP, rightElevation, EPSILON) && !Common::AreSame(ELEVATION_SINGULAR_POINT_DOWN, rightElevation, EPSILON))
-			{
-				rightAzimuth = rightVectorTo_sphereProjection.GetAzimuthDegrees();		//Get right azimuth
-			}
-
-
-			centerElevation = _vectorToListener.GetElevationDegrees();		//Get elevation from the head center
-			if (!Common::AreSame(ELEVATION_SINGULAR_POINT_UP, centerElevation, EPSILON) && !Common::AreSame(ELEVATION_SINGULAR_POINT_DOWN, centerElevation, EPSILON))
-			{
-				centerAzimuth = _vectorToListener.GetAzimuthDegrees();		//Get azimuth from the head center
-			}
-
-			interauralAzimuth = _vectorToListener.GetInterauralAzimuthDegrees();	//Get Interaural Azimuth
-
-		}
+		//	rightElevation = rightVectorTo_sphereProjection.GetElevationDegrees();	//Get right elevation	
+		//	if (!Common::AreSame(ELEVATION_SINGULAR_POINT_UP, rightElevation, EPSILON) && !Common::AreSame(ELEVATION_SINGULAR_POINT_DOWN, rightElevation, EPSILON))
+		//	{
+		//		rightAzimuth = rightVectorTo_sphereProjection.GetAzimuthDegrees();		//Get right azimuth
+		//	}
 
 
-		// In orther to obtain the position where the HRIR is needed, this method calculate the projection of each ear in the sphere where the HRTF has been measured
-		const Common::CVector3 GetSphereProjectionPosition(Common::CVector3 vectorToEar, Common::CVector3 earLocalPosition, float distance) const
-		{
-			//get axis according to the defined convention
-			float rightAxis = vectorToEar.GetAxis(RIGHT_AXIS);
-			float forwardAxis = vectorToEar.GetAxis(FORWARD_AXIS);
-			float upAxis = vectorToEar.GetAxis(UP_AXIS);
-			// Error handler:
-			if ((rightAxis == 0.0f) && (forwardAxis == 0.0f) && (upAxis == 0.0f)) {
-				ASSERT(false, RESULT_ERROR_DIVBYZERO, "Axes are not correctly set. Please, check axis conventions", "Azimuth computed from vector succesfully");
-			}
-			//get ear position in right axis
-			float earRightAxis = earLocalPosition.GetAxis(RIGHT_AXIS);
+		//	centerElevation = _vectorToListener.GetElevationDegrees();		//Get elevation from the head center
+		//	if (!Common::AreSame(ELEVATION_SINGULAR_POINT_UP, centerElevation, EPSILON) && !Common::AreSame(ELEVATION_SINGULAR_POINT_DOWN, centerElevation, EPSILON))
+		//	{
+		//		centerAzimuth = _vectorToListener.GetAzimuthDegrees();		//Get azimuth from the head center
+		//	}
 
-			//Resolve a quadratic equation to get lambda, which is the parameter that define the line between the ear and the sphere, passing by the source
-			// (x_sphere, y_sphere, z_sphere) = earLocalPosition + lambda * vectorToEar 
-			// x_sphere^2 + y_sphere^2 + z_sphere^2 = distance^2
+		//	interauralAzimuth = _vectorToListener.GetInterauralAzimuthDegrees();	//Get Interaural Azimuth
+
+		//}
 
 
-			float a = forwardAxis * forwardAxis + rightAxis * rightAxis + upAxis * upAxis;
-			float b = 2.0f * earRightAxis * rightAxis;
-			float c = earRightAxis * earRightAxis - distance * distance;
-			float lambda = (-b + sqrt(b * b - 4.0f * a * c)) * 0.5f * (1 / a);
+		//// In orther to obtain the position where the HRIR is needed, this method calculate the projection of each ear in the sphere where the HRTF has been measured
+		//const Common::CVector3 GetSphereProjectionPosition(Common::CVector3 vectorToEar, Common::CVector3 earLocalPosition, float distance) const
+		//{
+		//	//get axis according to the defined convention
+		//	float rightAxis = vectorToEar.GetAxis(RIGHT_AXIS);
+		//	float forwardAxis = vectorToEar.GetAxis(FORWARD_AXIS);
+		//	float upAxis = vectorToEar.GetAxis(UP_AXIS);
+		//	// Error handler:
+		//	if ((rightAxis == 0.0f) && (forwardAxis == 0.0f) && (upAxis == 0.0f)) {
+		//		ASSERT(false, RESULT_ERROR_DIVBYZERO, "Axes are not correctly set. Please, check axis conventions", "Azimuth computed from vector succesfully");
+		//	}
+		//	//get ear position in right axis
+		//	float earRightAxis = earLocalPosition.GetAxis(RIGHT_AXIS);
 
-			Common::CVector3 cartesianposition;
+		//	//Resolve a quadratic equation to get lambda, which is the parameter that define the line between the ear and the sphere, passing by the source
+		//	// (x_sphere, y_sphere, z_sphere) = earLocalPosition + lambda * vectorToEar 
+		//	// x_sphere^2 + y_sphere^2 + z_sphere^2 = distance^2
 
-			cartesianposition.SetAxis(FORWARD_AXIS, lambda * forwardAxis);
-			cartesianposition.SetAxis(RIGHT_AXIS, (earRightAxis + lambda * rightAxis));
-			cartesianposition.SetAxis(UP_AXIS, lambda * upAxis);
 
-			return cartesianposition;
-		}
+		//	float a = forwardAxis * forwardAxis + rightAxis * rightAxis + upAxis * upAxis;
+		//	float b = 2.0f * earRightAxis * rightAxis;
+		//	float c = earRightAxis * earRightAxis - distance * distance;
+		//	float lambda = (-b + sqrt(b * b - 4.0f * a * c)) * 0.5f * (1 / a);
 
-		/** \brief Get position and orientation of one listener ear
-		*	\param [in] ear listener ear for wich we want to get transform
-		*   \param [in] listenerTransform position of the listener
-		* 	\param [in] _listenerHRTF ptr to the HRTF of the listener
-		*	\retval transform current listener ear position and orientation
-		*   \eh On error, an error code is reported to the error handler.
-		*/
-		Common::CTransform GetListenerEarTransform(Common::T_ear ear, Common::CTransform& listenerTransform, std::shared_ptr<BRTServices::CHRTF>& listenerHRTF) const
-		{
-			return listenerTransform.GetLocalTranslation(listenerHRTF->GetEarLocalPosition(ear));
-		}
+		//	Common::CVector3 cartesianposition;
 
+		//	cartesianposition.SetAxis(FORWARD_AXIS, lambda * forwardAxis);
+		//	cartesianposition.SetAxis(RIGHT_AXIS, (earRightAxis + lambda * rightAxis));
+		//	cartesianposition.SetAxis(UP_AXIS, lambda * upAxis);
+
+		//	return cartesianposition;
+		//}		
 	};
 }
 #endif
