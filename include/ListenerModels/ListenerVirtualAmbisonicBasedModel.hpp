df30ac63a3dbffe71009e11e5aae85262b5a563d
Fran Morales <franmb@uma.es>
2024-06-25 12:32:25 +0200

    fix: initialize enable near field effect to false

--- include/ListenerModels/ListenerVirtualAmbisonicBasedModel.hpp
+++ include/ListenerModels/ListenerVirtualAmbisonicBasedModel.hpp
@@ -94,7 +94,7 @@ namespace BRTListenerModel {
 
 	public:
 		CListenerVirtualAmbisonicBasedModel(std::string _listenerID, BRTBase::CBRTManager* _brtManager) : brtManager{ _brtManager }, BRTBase::CListenerModelBase(_listenerID),
-			ambisonicOrder{ 1 }, ambisonicNormalization{ Common::TAmbisonicNormalization::N3D } {
+			ambisonicOrder{ 1 }, ambisonicNormalization{ Common::TAmbisonicNormalization::N3D }, enableNearFieldEffect{ false }{
 			
 			listenerHRTF = std::make_shared<BRTServices::CHRTF>();					// Create a empty HRTF	class
 			listenerAmbisonicIR = std::make_shared<BRTServices::CAmbisonicBIR>();	// Create a empty AmbisonicIR class
