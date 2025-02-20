0311788bc1843c804d879a10860db96c4260cdb8
Daniel González-Toledo <dgonzalezt@uma.es>
2025-02-20 14:21:02 +0100

    fix: Fix headers

--- include/Common/CranicalGeometry.hpp
+++ include/Common/CranicalGeometry.hpp
@@ -12,12 +12,11 @@
 * 
 * \b Contributions: (additional authors/contributors can be added here)
 *
-* \b Project: 3D Tune-In (https://www.3dtunein.eu) and SONICOM (https://www.sonicom.eu/) ||
+* \b Project: SONICOM (https://www.sonicom.eu/) ||
 *
-* \b Acknowledgement: This project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreements no. 644051 and no. 101017743
+* \b Acknowledgement: This project has received funding from the European Union's Horizon 2020 research and innovation programme under grant agreements no. 101017743
 * 
 * This class is part of the Binaural Rendering Toolbox (BRT), coordinated by A. Reyes-Lecuona (areyes@uma.es) and L. Picinali (l.picinali@imperial.ac.uk)
-* Code based in the 3DTI Toolkit library (https://github.com/3DTune-In/3dti_AudioToolkit).
 * 
 * \b Licence: This program is free software, you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 */
