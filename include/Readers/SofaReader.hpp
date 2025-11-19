8b80308a2beacaad0d97b64feb96d8a3a084e3da
Luis Molina-Tanco <lmtanco@users.noreply.github.com>
2025-11-19 13:05:11 +0100

    fix: filesystem path conversion to std::string

--- include/Readers/SofaReader.hpp
+++ include/Readers/SofaReader.hpp
@@ -1,4 +1,4 @@
-﻿/**
+/**
 * \class CSOFAReader
 *
 * \brief Declaration of CSOFAReader class
@@ -905,7 +905,7 @@ namespace BRTReaders {
 			}
 
 			std::filesystem::path p(sofafile);
-			std::string fileName{ p.filename().u8string() };
+			std::string fileName{ p.filename().string() };
 			dataHRTF->SetFilename(fileName);
 
 			if (_sourceUrl != "") {
