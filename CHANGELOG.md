7ef84c32e0ee1bfa363815f5b7002997ea58f2f8
Daniel González-Toledo <dgonzalezt@uma.es>
2026-07-23 12:39:54 +0200

    fix: When creating new sources, erroneous connections were repeatedly established between the ambisonic encoders and the decoders/convolvers

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,7 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 - Fixes a bug in SphericalInterpolatedFIRTable. The table with the common delay removed was not being saved.
-- Fixes bug in Listener-Ambisonic-RVL-model. When creating new sources, erroneous connections were repeatedly established between the ambisonic encoders and the decoders/convolvers.
+- Fixes bug in Listener-Ambisonics models. When creating new sources, erroneous connections were repeatedly established between the ambisonic encoders and the decoders/convolvers.
 
 ## [3.0.7] - 2026-07-07
 
--- include/ListenerModels/ListenerAmbisonicVirtualLoudspeakersModel.hpp
+++ include/ListenerModels/ListenerAmbisonicVirtualLoudspeakersModel.hpp
@@ -597,9 +597,11 @@ namespace BRTListenerModel {
 
 			control = control && brtManager->ConnectModulesMultipleSamplesVectors(_newSourceProcessors.bilateralAmbisonicEncoderProcessor, "leftAmbisonicChannels", leftAmbisonicDomainConvolverProcessor, "inputChannels");
 			control = control && brtManager->ConnectModulesMultipleSamplesVectors(_newSourceProcessors.bilateralAmbisonicEncoderProcessor, "rightAmbisonicChannels", rightAmbisonicDomainConvolverProcessor, "inputChannels");
-
-			control = control && brtManager->ConnectModulesSamples(leftAmbisonicDomainConvolverProcessor, "outputSamples", this, "leftEar");
-			control = control && brtManager->ConnectModulesSamples(rightAmbisonicDomainConvolverProcessor, "outputSamples", this, "rightEar");
+			
+			if (sourcesConnectedProcessors.size() == 0) {
+				control = control && brtManager->ConnectModulesSamples(leftAmbisonicDomainConvolverProcessor, "outputSamples", this, "leftEar");
+				control = control && brtManager->ConnectModulesSamples(rightAmbisonicDomainConvolverProcessor, "outputSamples", this, "rightEar");
+			}
 
 			if (control) {
 				SetSourceProcessorsConfiguration(_newSourceProcessors);
@@ -628,10 +630,12 @@ namespace BRTListenerModel {
 			std::string _sourceID = _source->GetID();
 			auto it = std::find_if(sourcesConnectedProcessors.begin(), sourcesConnectedProcessors.end(), [&_sourceID](CSourceToBeProcessed& sourceProcessorItem) { return sourceProcessorItem.sourceID == _sourceID; });
 			if (it != sourcesConnectedProcessors.end()) {
-
-				bool control = brtManager->DisconnectModulesSamples(leftAmbisonicDomainConvolverProcessor, "outputSamples", this, "leftEar");
-				control = control && brtManager->DisconnectModulesSamples(rightAmbisonicDomainConvolverProcessor, "outputSamples", this, "rightEar");
-
+				
+				bool control = true; 
+				if (sourcesConnectedProcessors.size() == 1) {
+					control = brtManager->DisconnectModulesSamples(leftAmbisonicDomainConvolverProcessor, "outputSamples", this, "leftEar");
+					control = control && brtManager->DisconnectModulesSamples(rightAmbisonicDomainConvolverProcessor, "outputSamples", this, "rightEar");
+				}
 				control = control && brtManager->DisconnectModulesMultipleSamplesVectors(it->bilateralAmbisonicEncoderProcessor, "leftAmbisonicChannels", leftAmbisonicDomainConvolverProcessor, "inputChannels");
 				control = control && brtManager->DisconnectModulesMultipleSamplesVectors(it->bilateralAmbisonicEncoderProcessor, "rightAmbisonicChannels", rightAmbisonicDomainConvolverProcessor, "inputChannels");
 
